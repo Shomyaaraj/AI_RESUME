437a10e123331979ae96e5e558f4c6df0d6364bc
Shomyaaraj <shomya725@gmail.com>
2026-03-27 17:51:42 +0530

    first commit

---
+++ src/models/user.model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose")
+
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        unique: [true, "username already taken"],
+        required: true,
+    },
+
+    email:{
+        type: String,
+        unique:[true, "Account already exists with this Email address "]
+    },
+
+    passwod:{
+        type: String,
+        required: true
+    }
+})
+
+
+const userMOdel = mongoose.model("users", userSchema)
+
+module.exports=userMOdel
