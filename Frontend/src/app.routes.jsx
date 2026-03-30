89df9787a9b5d71ce4751d3df664b44c977e36f7
Shomyaaraj <shomya725@gmail.com>
2026-03-30 22:14:48 +0530

    Dummy

---
+++ Frontend/src/app.routes.jsx
@@ -0,0 +1,16 @@
+import {createBrowser, createBrowserRouter} from "react-router"
+import Login from "./features/auth/pages/Login"
+import Register from "./features/auth/pages/Register"
+
+
+
+export const router = createBrowserRouter = createBrowserRouter([
+    {
+        path: "/login",
+        element:<Login/>
+    },
+    {
+        path:"/register",
+        element:<Register/>
+    }
+])
