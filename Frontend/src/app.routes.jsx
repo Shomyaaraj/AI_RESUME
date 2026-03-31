d0df8ce6805628e7aa7abc66f8a9e075eb4e265c
Shomyaaraj <shomya725@gmail.com>
2026-03-31 11:40:07 +0530

    Button

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -1,10 +1,10 @@
-import {createBrowser, createBrowserRouter} from "react-router"
+import { createBrowserRouter} from "react-router"
 import Login from "./features/auth/pages/Login"
 import Register from "./features/auth/pages/Register"
 
 
 
-export const router = createBrowserRouter = createBrowserRouter([
+export const router = createBrowserRouter([
     {
         path: "/login",
         element:<Login/>
--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import "../auth.form.scss"
 
 const Login = () => {
+
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+  }
+
+
   return (
-    <div>
-      Login
-    </div>
+    <main>
+      <div className="form-container">
+        <h1>Login</h1>
+
+        <form onSubmit={handleSubmit}> 
+
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" id='email' name='email' placeholder='Enter your email '/>
+          </div>
+
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" id='password' name='passwprd' placeholder='enter your password' />
+          </div>
+
+          <button className='button primary-button'>Login</button>
+        </form>
+      </div>
+    </main>
   )
 }
 
